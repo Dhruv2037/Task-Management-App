2a1559c825ea3f991d368c41b8795e170c3f1962
Dhruv2037 <dp2001.dd@gmail.com>
2024-06-12 17:37:18 -0400

    Backend commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
